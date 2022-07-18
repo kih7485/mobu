8da07df460e7403c9fb25ffcd922a54910b203e0
inhan <korea4127@gmail.com>
2022-07-19 00:27:20 +0900

    axios 커스텀 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material'
 import { theme } from '../theme'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from 'recoil';
 import '../styles/globals.css'
 import '@fullcalendar/common/main.css'
 import '@fullcalendar/daygrid/main.css'
 
+
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   )
 }
 
