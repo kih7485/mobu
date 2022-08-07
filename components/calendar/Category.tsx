89a0c9710aeb5a0606c41959594296f0ede8f3e1
inhan <korea4127@gmail.com>
2022-08-07 17:54:55 +0900

    카테고리 컬러 설정

--- components/calendar/Category.tsx
+++ components/calendar/Category.tsx
@@ -1,14 +1,62 @@
-import React from 'react'
-
+import React from 'react';
+import { Box, Paper, Button, ButtonProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { COLOR_OPTIONS } from '../../pages/api/common';
+import { purple } from '@mui/material/colors';
 const LOTTTO_LIST = [
     "아파트 분양",
     "APT 무순위/잔여세대",
     "오피스텔"
 ]
 
-function Category() {
-  return (
-    <div>{LOTTTO_LIST.map(data => (data))}</div>
+const CategoyStyle = styled(Paper)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 5,
+    marginBottom: 5
+}));
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function Category({ types }: any) {
+    return (
+        <CategoyStyle>
+            {/* <Box sx={{ '& button': { m: 1 } }}> */}
+                {types.map((type: string, i:number) => { 
+                    const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+                        color: theme.palette.getContrastText(COLOR_OPTIONS[i]),
+                        backgroundColor: COLOR_OPTIONS[i],
+                        '&:hover': {
+                            backgroundColor: COLOR_OPTIONS[i],
+                            
+                        },
+                    }));
+                    return (
+                        <div key={type}>
+                            <ColorButton variant="outlined" size="large">
+                                {type}
+                            </ColorButton>
+                        </div>
+                        )
+                }
+                )}
+            {/* <Box gridColumn="span 8">
+                <Item>xs=8</Item>
+            </Box>
+            <Box gridColumn="span 4">
+                <Item>xs=4</Item>
+            </Box> */}
+            
+        {/* </Box> */}
+            
+      </CategoyStyle>
+    
+  
+        //   {types.map((type: string) => (type))}
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
 import { useEffect, useState } from "react";
 import FullCalendar, { DatesSetArg, EventInput } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; 
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import { NextPage } from "next";
-import getAPTLttotPblancDetail from './api/aptSale';
 import { useQuery, useQueries } from "@tanstack/react-query";
 import CalendarStyle from "../components/calendar/CalendarStyle";
 import APTLttotPblancDetail from "../types/APTLttotPblancDetail";
 
-import { Axios, AxiosError } from "axios";
-import getUrbtyOfctlLttotPblancDetail from "./api/officetels";
-import getRemndrLttotPblancDetail from "./api/reminders";
 import UrbtyOfctlLttotPblancDetail from "../types/UrbtyOfctlLttotPblancDetail";
 import RemndrLttotPblancDetail from "../types/RemndrLttotPblancDetail";
 import Category from "../components/calendar/Category";
@@ -25,35 +21,36 @@ const Home: NextPage = () => {
   const [events, setEvents] = useState<any[]>([]);  
   const {data: aptData, isLoading: aptLoading} = useQuery(['getEstatesSubscription'], estatesSubscription);
   // const {data: aptRemindData, isLoading: remindLoading} = useQuery(['getRemndrLttotPblancDetail'], getRemndrLttotPblancDetail);
-  const {data: officetelsData, isLoading: officetelsLoading} = useQuery(['getUrbtyOfctlLttotPblancDetail'], getUrbtyOfctlLttotPblancDetail);
+  // const {data: officetelsData, isLoading: officetelsLoading} = useQuery(['getUrbtyOfctlLttotPblancDetail'], getUrbtyOfctlLttotPblancDetail);
   
   if (aptLoading) return "로딩중...";
   if (!aptLoading) {
     console.log(aptData, "aptData"); 
     const list: any = [];
-    aptData?.map(({ data }) => {
+    const types = [];
+    aptData?.map(({ data, type }) => {
+      types.push(type);
       data.map(obj => {
         list.push(obj);
       })
     })
     return (
-      <>      <Container maxWidth="xl">
-          <Category/>
-      <CalendarStyle> 
+      <>
+        <Container maxWidth="xl">
+          <Category types={types} />
+          <CalendarStyle>
             <FullCalendar
               plugins={[dayGridPlugin]}
               locale={'ko'}
               events={list} 
               contentHeight={700}
               datesSet={(arg: DatesSetArg) => {
-                  setEvents([...events, { title: "additional", start: arg.start }]);
-                  }}
-              />
-
-        </CalendarStyle>
-        
-      </Container>
-        </>
+                setEvents([...events, { title: "additional", start: arg.start }]);
+              }}
+            />
+          </CalendarStyle>
+        </Container>
+      </>
     )
     // console.log(list, "list");
   }
