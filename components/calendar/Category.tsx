e54be575d821d5c0993a4048f64c55220280d046
inhan <korea4127@gmail.com>
2022-08-09 00:02:23 +0900

    모달 컴포넌트 추가

--- components/calendar/Category.tsx
+++ components/calendar/Category.tsx
@@ -3,11 +3,6 @@ import { Box, Paper, Button, ButtonProps } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { COLOR_OPTIONS } from '../../pages/api/common';
 import { purple } from '@mui/material/colors';
-const LOTTTO_LIST = [
-    "아파트 분양",
-    "APT 무순위/잔여세대",
-    "오피스텔"
-]
 
 const CategoyStyle = styled(Paper)(({ theme }) => ({
     display: 'flex',
@@ -23,46 +18,42 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 function Category({ types }: any) {
-    const [click, setClick] = useState(true);
-    const handleToggle = () => {
-        setClick(click => !click); // on,off 개념 boolean
+    const arr = types.map( (type,index) => {
+        return {type: type, color: COLOR_OPTIONS[index]}
+    })
+    const [pick, setPick] = useState(arr);
+    const [select, setSelect] = useState<string[]>([]);
+
+    const handleToggle = (_, type) => {
+        !select.includes(type)
+            ? setSelect((select) => [...select, type])
+            : setSelect(select.filter((button) => button !== type));
     }
     return (
         <CategoyStyle>
-            {/* <Box sx={{ '& button': { m: 1 } }}> */}
             {types.map((type: string, i: number) => { 
                 const ColorButton = styled(Button)<ButtonProps>(({ theme }) => {
-                    const textColor = click ? theme.palette.getContrastText(COLOR_OPTIONS[i]) : COLOR_OPTIONS[i];
-                    const backgroundColor = click ? COLOR_OPTIONS[i] : '#FFFFFF';
+                    const textColor = select.includes(type) ?  COLOR_OPTIONS[i] : theme.palette.getContrastText(COLOR_OPTIONS[i]);
+                    const backgroundColor = select.includes(type) ? '#FFFFFF' : COLOR_OPTIONS[i] ;
                     return ({
-                        // color: theme.palette.getContrastText(COLOR_OPTIONS[i]),
-                        color: textColor,
-                        // backgroundColor: COLOR_OPTIONS[i],
+                        color: textColor, 
                         backgroundColor: backgroundColor,
                         '&:hover': {
-                            backgroundColor: COLOR_OPTIONS[i],
+                            backgroundColor: backgroundColor,
                             
                         },
                     })
                 });
                  
-                    return (
-                        <div key={type}>
-                            <ColorButton variant="outlined" size="large" onClick={handleToggle}>
-                                {type}
-                            </ColorButton>
-                        </div>
-                        )
+                return (
+                    <div key={type} onClick={event => handleToggle(event, type)}>
+                        <ColorButton variant="outlined" size="large" >
+                            {type}
+                        </ColorButton>
+                    </div>
+                    )
                 }
                 )}
-            {/* <Box gridColumn="span 8">
-                <Item>xs=8</Item>
-            </Box>
-            <Box gridColumn="span 4">
-                <Item>xs=4</Item>
-            </Box> */}
-            
-        {/* </Box> */}
             
       </CategoyStyle>
     
