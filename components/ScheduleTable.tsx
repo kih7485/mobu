cd1e7fbe31fd78559a5e8694abb7f4cf5c2cd057
inhan <korea4127@gmail.com>
2022-08-24 22:53:29 +0900

    카테고리별 청약일정 테이블 분리

---
+++ components/ScheduleTable.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const TAX_RATE = 0.07;
+
+function ccyFormat(num: number) {
+  return `${num.toFixed(2)}`;
+}
+
+function priceRow(qty: number, unit: number) {
+  return qty * unit;
+}
+
+function createRow(desc: string, qty: number, unit: number) {
+  const price = priceRow(qty, unit);
+  return { desc, qty, unit, price };
+}
+
+interface Row {
+  desc: string;
+  qty: number;
+  unit: number;
+  price: number;
+}
+
+function subtotal(items: readonly Row[]) {
+  return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
+}
+
+const rows = [
+  createRow('Paperclips (Box)', 100, 1.15),
+  createRow('Paper (Case)', 10, 45.99),
+  createRow('Waste Basket', 2, 17.99),
+];
+
+const invoiceSubtotal = subtotal(rows);
+const invoiceTaxes = TAX_RATE * invoiceSubtotal;
+const invoiceTotal = invoiceTaxes + invoiceSubtotal;
+
+export default function ScheduleTable({ ...props }) {
+  const { HOUSE_SECD } : any = props;
+  
+  const aptTable = (
+    <div>
+
+    </div>
+  )
+
+  console.log(props,"프롭");
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="a dense table">
+                <TableBody>
+                    <TableRow>
+                        <TableCell width={'20%'}>공급위치</TableCell> 
+                        <TableCell align="left" >Qtyㄴㅁㄴㅇ.</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell width={'20%'}>공급규모</TableCell> 
+                        <TableCell align="left" >Qty.</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell width={'20%'}>입주자모집공고 관련 문의</TableCell> 
+                        <TableCell align="left">Qty.</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell width={'20%'}>문의처</TableCell> 
+                        <TableCell align="left">010-1234-5678</TableCell>
+                    </TableRow>
+          {/* {rows.map((row) => (
+            <TableRow key={row.desc}>
+              <TableCell>{row.desc}</TableCell>
+              <TableCell align="right">{row.qty}</TableCell>
+            </TableRow>
+          ))} */}
+          
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- components/modal/InformationModal.tsx
+++ components/modal/InformationModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Dialog,
@@ -15,6 +15,7 @@ import { EventApi, EventDef } from "@fullcalendar/common";
 import CloseIcon from '@mui/icons-material/Close';
 import SpanningTable from "../Table";
 import InfoIcon from '@mui/icons-material/Info';
+import ScheduleTable from "../ScheduleTable";
 
 export interface InformationModalProps {
     title?: string;
@@ -23,7 +24,12 @@ export interface InformationModalProps {
     handleClose?: (...arg: any[]) => any;
     handleInformation?: (...arg: any[]) => any;
 }
-
+// const HOUSE_SECD = {
+//     "01": "APT",
+//     "02": "도시형/오피스텔/민간임대",
+//     "04": "무순위/잔여세대",
+//     "06": "계약취소주택"
+// }
 const InformationModal = ({
     title,
     message,
@@ -32,14 +38,16 @@ const InformationModal = ({
     handleInformation
 }: InformationModalProps) => {
     const { hideModal } = useModal();
-    
-  
     const onClose = () => {
         if (handleClose) {
             handleClose();
         }
         hideModal();
     };
+    // if (eventDef?.extendedProps) {
+    //     setCategory(eventDef?.extendedProps.HOUSE_SECD);
+    // }
+    // console.log(category, "category");
     console.log(eventDef, "eventDef");
 //   const onConfirm = async () => {
 //     if (handleConfirm) {
@@ -66,7 +74,8 @@ const InformationModal = ({
             <SpanningTable />
           </DialogContent>
            <DialogContent>
-            <span><InfoIcon /> 청약일정</span>
+              <span><InfoIcon /> 청약일정</span>
+              <ScheduleTable {...eventDef?.extendedProps} />
           </DialogContent>
       <DialogActions>
         ㄴㄴㄴzz 
